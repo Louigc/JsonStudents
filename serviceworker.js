fc763de72016b64bf1e6de1ff4e00b1908af6571
Louise Christiansen <89901683+Louigc@users.noreply.github.com>
2022-10-04 10:33:47 +0200

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,15 @@
+const cacheName = 'cache-members';
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(['/JsonStudents/', '/JsonStudents/index.html', '/JsonStudents/members.json', '/JsonStudents/morten.png', '/JsonStudents/mystyle.css', '/JsonStudents/nina.png', '/JsonStudents/olivia.png']);
+    })
+  );
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.open(cacheName).then(cache => cache.match(event.request))
+    )
+  );
+});
